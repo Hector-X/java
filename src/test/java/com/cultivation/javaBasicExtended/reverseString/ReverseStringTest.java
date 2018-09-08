30ad7c06560e083abfeffeb489412c3948229368
YKGDWADE3 <superwadeg3@gmail.com>
2018-09-08 14:35:45 +0800

    [Kaiguang][javaBasic]pass ReverseStringTest

--- src/test/java/com/cultivation/javaBasicExtended/reverseString/ReverseStringTest.java
+++ src/test/java/com/cultivation/javaBasicExtended/reverseString/ReverseStringTest.java
@@ -2,9 +2,7 @@ package com.cultivation.javaBasicExtended.reverseString;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 /*
  * Difficulty: Easy
--- src/test/java/com/cultivation/javaBasicExtended/reverseString/StringReverser.java
+++ src/test/java/com/cultivation/javaBasicExtended/reverseString/StringReverser.java
@@ -1,13 +1,27 @@
 package com.cultivation.javaBasicExtended.reverseString;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 class StringReverser {
     @SuppressWarnings({"WeakerAccess", "unused"})
     public static String[] reverse(String input) {
         // TODO: please implement the method to pass all the tests.
         // <--start
-        throw new NotImplementedException();
+        if (input == null) {
+            throw new IllegalArgumentException();
+        }
+
+        String inputAfterProcess = input.trim();
+        if (inputAfterProcess.isEmpty()) return new String[0];
+
+        String[] reverseResult = inputAfterProcess.split(" ");
+
+        if (reverseResult.length < 2) return reverseResult;
+
+        for (int index = 0; index < reverseResult.length / 2; index++) {
+            String temp = reverseResult[index];
+            reverseResult[index] = reverseResult[reverseResult.length - 1 - index];
+            reverseResult[reverseResult.length - 1 - index] = temp;
+        }
+        return reverseResult;
         // --end-->
     }
 }
